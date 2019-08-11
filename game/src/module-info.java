d9640efa0014285ca0af76fc92571dedb08c8b3e
liyun <liyun_19811125@163.com>
2019-08-11 21:03:51 +0800

    更新模块声明文件module-info.java

--- game/src/module-info.java
+++ game/src/module-info.java
@@ -4,18 +4,6 @@ open module legend{
     requires static org.junit.jupiter.api;
 
     exports legend;
-    exports legend.intf;
-    exports legend.util;
-    exports legend.util.chardet;
-    exports legend.util.intf;
-    exports legend.util.entity;
-    exports legend.util.param;
-    exports legend.util.rule;
-    exports legend.util.rule.intf;
-    exports legend.helper;
-    exports legend.helper.entity;
-    exports legend.helper.intf;
-    exports legend.helper.il;
     exports legend.game.dos2;
     exports legend.game.dos2.entity;
     exports legend.game.dos2.intf;
@@ -29,4 +17,18 @@ open module legend{
     exports legend.game.run;
     exports legend.game.run.entity;
     exports legend.game.run.intf;
+    exports legend.helper;
+    exports legend.helper.entity;
+    exports legend.helper.intf;
+    exports legend.intf;
+    exports legend.util;
+    exports legend.util.adapter;
+    exports legend.util.chardet;
+    exports legend.util.entity;
+    exports legend.util.intf;
+    exports legend.util.logic;
+    exports legend.util.logic.intf;
+    exports legend.util.param;
+    exports legend.util.rule;
+    exports legend.util.rule.intf;
 }
